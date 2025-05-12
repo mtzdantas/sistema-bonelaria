30e4eb10133def3cf2ab7dfc957742ea2e4cf05d
Mateus Dantas <mtzdantas@gmail.com>
2025-05-12 12:19:18 -0300

    fix: muda nome para ficar igual ao arquivo .env

--- sistema/bonelaria_militar/lib/supabase/supabase_client.dart
+++ sistema/bonelaria_militar/lib/supabase/supabase_client.dart
@@ -6,7 +6,7 @@ class SupabaseConfig {
     await dotenv.load();
     await Supabase.initialize(
       url: dotenv.env['SUPABASE_URL']!,
-      anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+      anonKey: dotenv.env['SUPABASE_KEY']!,
     );
   }
 
